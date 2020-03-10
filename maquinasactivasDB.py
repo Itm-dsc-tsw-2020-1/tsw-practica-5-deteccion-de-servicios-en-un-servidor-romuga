fe54d1968de46fce98e712da322df70b1bf5f0b3
romuga <rossmg1996@gmail.com>
2020-03-10 12:01:03 -0600

    Practica 5 ROSARIO MUÑOZ

---
+++ maquinasactivasDB.py
@@ -0,0 +1,37 @@
+import os
+import mysql.connector as mysql
+from subprocess import Popen, PIPE
+import subprocess
+#ROSARIO MUÑOZ GARCÍA ITIC's
+#Conectandose a base de datos
+
+print ("Python conectándose a MySQL")
+conexion = mysql.connect( host='localhost', user= 'root', passwd='', db='puertosCompu' )
+operacion = conexion.cursor()
+
+computadora = "200.33.171.124"
+comando = "nmap -sT  200.33.171.124"
+
+#resultado = os.popen(comando)
+#puertos = resultado.readlines()
+
+archivo = open ('puertos.txt')
+texto = archivo.readlines()
+archivo.close()
+#print(texto)
+texto2 = texto
+sql = "INSERT INTO Computadoras (IP, Puerto, Estado, Servicio) VALUES (%s, %s, %s, %s)"
+print(texto2)
+for puerto in texto2[6:len(texto2)-2]:
+    print(puerto.split())
+    operacion.execute(sql, (computadora, puerto.split()[0],puerto.split()[1],puerto.split()[2]))
+    conexion.commit()
+    #operacion.execute( 'INSERT INTO Computadoras (IP, Puerto, Estado, Servicio) VALUES (200.33.171.124,'+puerto.split()[0]+','+puerto.split()[1]+','+puerto.split()[2]) 
+    
+    #print(puerto.split())
+conexion.close()
+
+
+"""red = "200.33.171.0/24"
+os.system("nmap -sT  -v " + red )
+"""
